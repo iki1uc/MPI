6d7ae25ccba24b404d64882ae27d39f349a3b558
iki1uc <entertrainer@web.de>
2026-08-09 20:35:49 +0200

    Update Resp.js

--- Resp.js
+++ Resp.js
@@ -1,3 +1,13 @@
-<div class="response">
-  OK / NOK / FLOW / BREAK / ECHO / VOID / ROOT
-</div>
+export function mapMPIto4u(bet) {
+
+    const map = {
+        fix: "ROOT",
+        flow: "FLOW",
+        expand: "OK",
+        charge: "ECHO",
+        shield: "VOID",
+        release: "BREAK"
+    };
+
+    return map[bet] || "NOK";
+}
