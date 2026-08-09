26d69e0e1ef1e4c395940a33db487c4f31b95d29
iki1uc <entertrainer@web.de>
2026-08-09 20:35:34 +0200

    Update Math.js

--- Math.js
+++ Math.js
@@ -1,6 +1,36 @@
-<math display="block">
-  <mrow>
-    <msup><mn>3</mn><mn>3</mn></msup>
-    <mo>=</mo><mn>27</mn>
-  </mrow>
-</math>
+export function calcMPIState(temp, pressure, soll, ist) {
+
+    // Gewichtung
+    const wTemp = temp * 0.4;
+    const wPress = pressure * 0.3;
+    const wDiff = (ist - soll) * 0.3;
+
+    // Score (0–100)
+    const score = wTemp + wPress + wDiff;
+
+    // Wette (Vorhersage)
+    let bet = "neutral";
+
+    if (score < 10) bet = "fix";
+    else if (score < 30) bet = "flow";
+    else if (score < 50) bet = "expand";
+    else if (score < 70) bet = "charge";
+    else if (score < 90) bet = "shield";
+    else bet = "release";
+
+    // MPI‑State aus Wette
+    const stateMap = {
+        fix: "FIX-I",
+        flow: "FLOW-I",
+        expand: "EXPAND-I",
+        charge: "CHARGE-I",
+        shield: "SHIELD-I",
+        release: "RELEASE-I"
+    };
+
+    return {
+        score,
+        bet,
+        mpi: stateMap[bet]
+    };
+}
